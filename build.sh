20c929b2658e0a51cdc97385f441b0f702a4d476
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2025-05-24 09:05:22 -0300

    Added install of dependencies for linux

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install libxrandr-dev libxcursor-dev libxi-dev libudev-dev libflac-dev libvorbis-dev libgl1-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev libfreetype-dev
+#sudo apt install libxrandr-dev libxcursor-dev libxi-dev libudev-dev libflac-dev libvorbis-dev libgl1-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev libfreetype-dev
 cmake -S . -B build
 cmake --build build --config Release --parallel 4
 
